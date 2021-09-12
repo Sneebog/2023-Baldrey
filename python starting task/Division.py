4ae423ef8ba71d44404e65beeb6e428474cb6cc7
Sneebog <Sethbaldrey@gmail.com>
2021-09-12 16:35:17 +0100

    Create Division.py

---
+++ python starting task/Division.py
@@ -0,0 +1,7 @@
+print("what is integer?")
+integer = int(input())
+print("what is divisor")
+divisor = int(input())
+division = integer // divisor
+remainder = integer % divisor
+print(division, "remainder", remainder)
