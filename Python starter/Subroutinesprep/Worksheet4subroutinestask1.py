6c63f3ee78b73be35fb3c6ce04f06bb29e6f11ed
Sneebog <Sethbaldrey@gmail.com>
2021-11-23 15:00:45 +0000

    Create Worksheet4subroutinestask1.py

---
+++ Python starter/Subroutinesprep/Worksheet4subroutinestask1.py
@@ -0,0 +1,12 @@
+def multiples(f1,f2,f3,f4):
+    print("Hi", f4, "here is yout times table")
+    for i in range(f2,f3+1):
+        print(f1, "x", i, "=", f1 * i)
+    next
+#main program
+pupilname = input("What is your name?")
+print("enter times table, start number and end number")
+table = int(input())
+startnum = int(input())
+endnum = int(input())
+multiples(table,startnum,endnum,pupilname)
