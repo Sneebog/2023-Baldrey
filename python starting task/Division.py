84353a5f53c61eae690690377bc7e94259281158
Sneebog <Sethbaldrey@gmail.com>
2021-11-23 15:00:31 +0000

    Delete Division.py

--- python starting task/Division.py
+++
@@ -1,7 +0,0 @@
-print("what is integer?")
-integer = int(input())
-print("what is divisor")
-divisor = int(input())
-division = integer // divisor
-remainder = integer % divisor
-print(division, "remainder", remainder)
