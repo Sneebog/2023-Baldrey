0fa5a1d245f9f1be21fc5f46052ed832803fe35c
Sneebog <Sethbaldrey@gmail.com>
2021-11-26 13:57:24 +0000

    Update Brick Breaker.py

--- Brick Breaker folder/Brick Breaker.py
+++ Brick Breaker folder/Brick Breaker.py
@@ -6,12 +6,13 @@ WHITE = (255, 255, 255)
 RED = (255,   0,   0)
 BLUE = (0, 0, 255)
 FUNNY = (69, 42, 69)
-CHIMBUS = (80, 0, 80)
+CHIMBUS = (80, 160, 80)
 PLIMBEY = (145, 141, 12)
 BACKGROUND = (23, 255, 247)
 PINKY = (230, 41, 173)
 BORDER = (63, 65, 69)
 colour = RED
+player_color = RED
 #define some variables
 x_speed = 0
 playerx = 250
@@ -20,6 +21,8 @@ answer = ""
 speedtimer = 0
 xoffset, yoffset = 3, 3
 nonraxoffset, nonrayoffset = 3, 3
+
+#Create Classes
 class Block(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
@@ -52,50 +55,53 @@ class player(pygame.sprite.Sprite):
             return 2   
         else:
             return 1
-
 class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
-Background = Background("cringus.png", [0,100])
+#Create Functions
+def write(text, color, size, fontPath):
+        font = pygame.font.Font(fontPath, size)
+        text = font.render(text, 1, pygame.Color(color))
+        return 
+Background = Background("stimbo.jpg", [0,80])
 # Initialize Pygame
 pygame.init()
 # Set the height and width of the screen
 screen_width = 500
-screen_height = 700
+screen_height = 650
 screen = pygame.display.set_mode([screen_width, screen_height])
+#create fonts
+outfit = pygame.font.SysFont('Outfit-Bold.ttf', 35)
 #name the window and play the background music
 pygame.display.set_caption("Project Plimble")
-pygame.mixer.init()
-pygame.mixer.music.load("videogame.mp3")
-pygame.mixer.music.play(-1,0.0)
 #Make the sprite lists
 block_list = pygame.sprite.Group()
 all_sprites_list = pygame.sprite.Group()
-for i in range(5, 11):
+for i in range(4, 10):
     powerupcolour = random.randint(1,15)
-    if i == 5:
+    if i == 4:
         colour = RED
-    elif i == 6:
+    elif i == 5:
         colour =  BLUE
-    elif i == 7:
+    elif i == 6:
         colour = BLACK
-    elif i == 8:
+    elif i == 7:
         colour = FUNNY
-    elif i == 9:
+    elif i == 8:
         colour = CHIMBUS
-    elif i == 10:
+    elif i == 9:
         colour = PINKY
-    for j in range(14):
+    for j in range(12):
         colourblock = colour
         if j == powerupcolour:
             colourblock = PLIMBEY
         # This represents a block
-        block = Block(colourblock, 37, 15)
+        block = Block(colourblock, 38, 15)
         # Set a location for the block
-        block.rect.x = ((j * 39) + 10)
+        block.rect.x = ((j * 40) + 10)
         block.rect.y = (i * 20)
         # Add the block to the list of objects
         block_list.add(block)
@@ -117,9 +123,9 @@ while not done:
     if len(block_list) == 0:
         done = True
         answer = "congratulation"
-    #elif yball > 385:
-        #done = True
-        #answer = "you are awful"
+    elif yball > 625:
+        done = True
+        answer = "you are awful"
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             done = True
@@ -145,7 +151,17 @@ while not done:
     pygame.draw.rect(screen,BORDER, [0, 0, 10, 700 ])
     pygame.draw.rect(screen,BORDER, [490, 0, 10, 700 ])
     pygame.draw.rect(screen,BORDER, [0, 0, 500, 10 ])
-    pygame.draw.rect(screen,BORDER, [0, 690, 500, 10 ])
+    pygame.draw.rect(screen,BORDER, [0, 640, 500, 10 ])
+    pygame.draw.rect(screen,BORDER, [0, 70, 500, 10 ])
+    #draw the scoreboards and the title
+    score_board = outfit.render("SCORE: " +  str(score),  True, BLUE)
+    highscore_board = outfit.render("HIGHSCORE: " + str(7 * 12 * 20),  True, BLUE)
+    screen.blit(score_board, (250, 20))
+    screen.blit(highscore_board, (250, 40))
+    title1 = outfit.render("STUNNA", True, RED)
+    title2 = outfit.render("BREAKER", True, RED)
+    screen.blit(title1, (60, 20))
+    screen.blit(title2, (50, 40))
     #stop the paddles going off the screen
     if  (player.rect.x) - 5 <= 0:
         playerx += 5
@@ -164,12 +180,12 @@ while not done:
     #make the ball bounce off the screen
     if xball < 10 or xball >= 480:
         xoffset = xoffset * -1
-    if yball < 10 or yball >= 670:
+    if yball < 80 or yball >= 630:
         yoffset = yoffset * -1
     # See if the player block has collided with anything.
     blocks_hit_list = pygame.sprite.spritecollide(ball, block_list, True)
     #Make ball bounce of padde(change this to spritecollide)
-    if ((ball.rect.y > (player.rect.y - 10)) and (ball.rect.y < (player.rect.y + 10))) and ((ball.rect.x > player.rect.x - 10) and (ball.rect.x < player.rect.x + 70)):
+    if ((ball.rect.y > (player.rect.y - 15)) and (ball.rect.y < (player.rect.y + 50))) and ((ball.rect.x > player.rect.x - 15) and (ball.rect.x < player.rect.x + 50)):
         if x_speed == -5:
              xoffset = xoffset * -1
         yoffset = yoffset * -1 
@@ -185,9 +201,12 @@ while not done:
         speedtimer -= 1
     if speedtimer == 0:
         player.abilities(1)
-    #add a scoreboard
+    #change colour for powerup
+    if speedtimer > 0:
+        player.color = BLUE
+    #add a scoreboardblock in blocks_hit_list:
     for block in blocks_hit_list:
-        score += 1
+        score += 20
     # Draw all the spites
     all_sprites_list.draw(screen)
     # Go ahead and update the screen with what we've drawn.
@@ -195,5 +214,82 @@ while not done:
     # Limit to 60 frames per second
     clock.tick(60)
 #display the outcome
-print(answer)
 pygame.quit()
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+gameover_timer = 0
+if answer == "you are awful":
+    gameover = False 
+    # Initialize Pygame
+    pygame.init()
+    outfitreg = pygame.font.SysFont('Outfit-Regular.ttf', 75)
+    clock = pygame.time.Clock()
+    #name the window and play the background music
+    pygame.display.set_caption("Game over screen 1")
+    pygame.mixer.init()
+    pygame.mixer.music.load("Lose music.mp3")
+    pygame.mixer.music.play(-1,0.0)
+    # Set the height and width of the screen
+    Background = Background("Lose screen.jpg", [0,0])
+    # Set the height and width of the screen
+    screen_width = 500
+    screen_height = 650
+    screen = pygame.display.set_mode([screen_width, screen_height])
+    while not gameover:
+        if gameover_timer == 600:
+            gameover = True
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                gameover = True
+        # Clear the screen
+        screen.fill(BLACK)
+        #set the background image
+        screen.blit(Background.image, Background.rect)
+        Gameoverscreen = outfitreg.render("GAME OVER", True, RED)
+        screen.blit(Gameoverscreen, (125, 250))
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+        # Limit to 60 frames per second
+        clock.tick(60)
+        gameover_timer += 1
+    pygame.quit
+elif answer == "congratulation":
+    gameover = False 
+    # Initialize Pygame
+    pygame.init()
+    outfitreg = pygame.font.SysFont('Outfit-Regular.ttf', 75)
+    clock = pygame.time.Clock()
+    #name the window and play the background music
+    pygame.display.set_caption("Game over screen 24")
+    pygame.mixer.init()
+    pygame.mixer.music.load("congratulations.mp3")
+    pygame.mixer.music.play(-1,0.0)
+    # Set the height and width of the screen
+    Background = Background("Win screen.jpg", [0,0])
+    screen_width = 500
+    screen_height = 550
+    screen = pygame.display.set_mode([screen_width, screen_height])
+    while not gameover:
+        if gameover_timer == 600:
+            gameover = True
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                gameover = True
+        # Clear the screen
+        screen.fill(BLACK)
+        #set the background image
+        screen.blit(Background.image, Background.rect)
+        Gameoverscreen = outfitreg.render("Congatulations", True, RED)
+        screen.blit(Gameoverscreen, (100, 250))
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+        # Limit to 60 frames per second
+        clock.tick(60)
+        gameover_timer += 1
+    pygame.quit
+print(answer)
